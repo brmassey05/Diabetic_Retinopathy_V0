a6c313e15b7ad8f8c0b663cce9fb429bca1feb99
Brendan Massey <masseybr@alumni.upenn.edu>
2023-03-14 14:42:18 -0400

    Fixing bug

--- Version_Zero/Main.py
+++ Version_Zero/Main.py
@@ -22,7 +22,7 @@ def retrieve_data():
   print(len(data))
   return trainLabels
 
-"""TO DO: Modify this to match the current dataset being utilized."""
+"""TO DO: Modify this to match the current dataset being utilized.
 class HemorrhageDataset(torch.utils.data.Dataset):
   def __init__(self, train: bool = True):
     split = "train" if train else "test"
@@ -171,7 +171,7 @@ class Trainer():
     for epoch in range(1, n_epochs + 1):
       self.train_epoch(epoch)
       self.test_epoch(epoch)
-"""
+
 # Parameters
 n_epochs = 10
 batch_size = 32
